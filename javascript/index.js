4fbf615eecac1c46e34b529ffb08dd52535c2e71
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-02-20 19:25:36 +0100

    fremside og alle filmer siden

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ import { listData } from "./utils.js"
 
 
 let collection = [];
-const outElement = document.getElementById("movieList");
+const outElement = document.getElementById("movieTop");
 
 async function collectMovies() {
     try {
@@ -12,10 +12,14 @@ async function collectMovies() {
 
         collection = data.data;
 
-        listData(collection, outElement);
+        const movieIdsToDisplay = ["04fd79ad-2612-4dab-b2ee-1320c4e5ccd1", "352ba432-5b5d-4ccc-9aba-f2704c500cf3", "4696b9e6-ec6e-4672-a08d-3e3212a215c8", "581f13b2-3ca4-494e-be7c-bb51fbc320f4"];
+
+        const selectedMovies = collection.filter(movie => movieIdsToDisplay.includes(movie.id));
+
+        listData(selectedMovies, outElement);
     } catch(error) {
-        console.error(`Could not fetch data...`, error)
-        outElement.innerHTML = `Could not fetch data...`
+        console.error(`Could not fetch data...`, error);
+        outElement.innerHTML = `Could not fetch data...`;
     }
 }
 
